de3777ca1bbaf70b9acea40fd6e5e455397e8c1c
lambrou <alexanderlambrou0602@gmail.com>
2019-09-28 13:51:38 +0000

    Added ability to add food eaten. Updates daily calorie, carb, and fat content.

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -39,37 +39,46 @@ post '/users/:user_id' do
   else
     erb :not_authorized
   end
-  
+
   @user = User.find(session[:user_id])
-  bmrl = { "Sedentary" => 1.2, "Lightly Active" => 1.375, "Moderately Active" => 1.55, "Very Active" => 1.725, "Highly Active" => 1.9 }
-  bmi = (params[:weight].to_f/(params[:height].to_f**2) * 703.0)
-  bmr = 0
-  if params[:sex]
-    @user.sex = params[:sex]
-  end
-  if @user.sex or params[:sex]
-    if @user.sex == 'Male'
-      bmr = (66 + (6.3 * params[:weight].to_f) + (params[:height].to_f * 12.9) - (6.8 * params[:age].to_f))
-    else
-      bmr = (655 + (4.3 * params[:weight].to_f) + (params[:height].to_f * 4.7) - (4.7 * params[:age].to_f))
+
+  if params[:weight]
+    bmrl = { "Sedentary" => 1.2, "Lightly Active" => 1.375, "Moderately Active" => 1.55, "Very Active" => 1.725, "Highly Active" => 1.9 }
+    bmi = (params[:weight].to_f/(params[:height].to_f**2) * 703.0)
+    bmr = 0
+    if params[:sex]
+      @user.sex = params[:sex]
     end
+    if @user.sex or params[:sex]
+      if @user.sex == 'Male'
+        bmr = (66 + (6.3 * params[:weight].to_f) + (params[:height].to_f * 12.9) - (6.8 * params[:age].to_f))
+      else
+        bmr = (655 + (4.3 * params[:weight].to_f) + (params[:height].to_f * 4.7) - (4.7 * params[:age].to_f))
+      end
+    end
+
+    @user.bmi = '%.2f' % bmi
+    @user.bmr = bmr
+    
+    if params[:lifestyle]
+      @user.lifestyle = params[:lifestyle]
+    end
+
+    @user.maxcal = (bmr * bmrl[@user.lifestyle]) - 500
+    @user.age = params[:age]
+    @user.weight = params[:weight]
+    @user.tweight = params[:tweight]
+    @user.height = params[:height]
+
   end
 
-  @user.bmi = '%.2f' % bmi
-  @user.bmr = bmr
+  if params[:calcontent]
+    @user.calories = @user.calories + params[:calcontent].to_i
+    @user.carbs = @user.carbs + params[:carbcontent].to_i
+    @user.fat = @user.fat + params[:fatcontent].to_i
+  end
   
-  if params[:lifestyle]
-    @user.lifestyle = params[:lifestyle]
-  end
-
-  @user.maxcal = (bmr * bmrl[@user.lifestyle]) - 500
-  @user.age = params[:age]
-  @user.weight = params[:weight]
-  @user.tweight = params[:tweight]
-  @user.height = params[:height]
-
   if @user.save
     redirect '/users/' + session[:user_id].to_s
   end
-
 end
